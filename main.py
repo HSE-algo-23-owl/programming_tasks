1bc9974c81ee6025b67326b092b65f2768f7dffb
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2023-09-13 20:15:46 +0500

    Добавил задание 1

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-def hello() -> str:
-    """Возвращает приветствие Hello World!"""
+import time
+
+
+def fibonacci_rec(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована рекурсивно.
+
+    :param n: порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
+    pass
+
+
+def fibonacci_iter(n: int) -> int:
+    """Возвращает N-е число Фибоначчи. Реализована итеративно.
+
+    :param n: порядковый номер числа Фибоначчи
+    :return: число Фибоначчи
+    """
     pass
 
 
 def main():
-    print(hello())
+    n = 35
+    print(f'Вычисление {n} числа Фибоначчи рекурсивно:')
+    start_time = time.time()
+    print(fibonacci_rec(n))
+    print(f'duration: {time.time() - start_time} seconds')
+
+    print(f'Вычисление {n} числа Фибоначчи итеративно:')
+    start_time = time.time()
+    print(fibonacci_iter(n))
+    print(f'duration: {time.time() - start_time} seconds')
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import unittest
 
+from main import fibonacci_rec, fibonacci_iter
 
-from main import hello
 
+class TestFibonacci(unittest.TestCase):
+    """Тесты для проверки функций вычисления числа Фибоначчи"""
+    fibonacci_numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377,
+                         610, 987, 1597, 2584, 4181, 6765, 10946, 17711]
 
-class TestHello(unittest.TestCase):
-    def test_hello(self):
-        self.assertEqual(hello(), 'Hello world!')
+    def test_fibonacci_rec(self):
+        """Проверка работы рекурсивной функции вычисления числа Фибоначчи"""
+        for index, number in enumerate(self.fibonacci_numbers):
+            self.assertEqual(fibonacci_rec(index + 1), number)
+
+    def test_fibonacci_iter(self):
+        """Проверка работы итеративной функции вычисления числа Фибоначчи"""
+        for index, number in enumerate(self.fibonacci_numbers):
+            self.assertEqual(fibonacci_iter(index + 1), number)
 
 
 if __name__ == '__main__':
