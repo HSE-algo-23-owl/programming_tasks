cca1317bcdc6f25af0b915ea355cb04d324a60a3
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2023-09-14 00:02:25 +0500

    Добавил доп задание

--- main.py
+++ main.py
@@ -19,6 +19,19 @@ def fibonacci_iter(n: int) -> int:
     pass
 
 
+def rabbits(month: int, lifetime: int) -> int:
+    """Возвращает количество пар кроликов в популяции на заданный месяц.
+    В начальный момент времени имеется одна пара кроликов. Начиная со второго
+    месяца после рождения кролики производят новую пару кроликов каждый месяц.
+    После достижения предельного возраста кролики умирают.
+
+    :param month: количество месяцев жизни популяции
+    :param lifetime: продолжительность жизни кролика, не менее 2 месяцев
+    :return: количество пар кроликов
+    """
+    pass
+
+
 def main():
     n = 35
     print(f'Вычисление {n} числа Фибоначчи рекурсивно:')
@@ -26,10 +39,16 @@ def main():
     print(fibonacci_rec(n))
     print(f'duration: {time.time() - start_time} seconds')
 
-    print(f'Вычисление {n} числа Фибоначчи итеративно:')
+    print(f'\nВычисление {n} числа Фибоначчи итеративно:')
     start_time = time.time()
     print(fibonacci_iter(n))
     print(f'duration: {time.time() - start_time} seconds')
+
+    if rabbits(1, 2):
+        lifetime = 5
+        print(f'\nВычисление числа пар кроликов по состоянию на {n} месяц')
+        print(f'при продолжительности жизни кролика {lifetime} месяцев')
+        print(rabbits(n, lifetime))
 
 
 if __name__ == '__main__':
