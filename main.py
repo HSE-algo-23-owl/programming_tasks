d71ffb4fdcf9eee80c9e3c4bd607e7301f035955
Timofei <tscherepanov@edu.hse.ru>
2023-09-25 20:51:50 +0500

    Realized functions: fibonacci_rec and fibonacci_iter. Rabbits function in development

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import time
 
 def fibonacci_rec(n: int) -> int:
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно.
-
+        1  1  2 3 5
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if type(n) != int or n < 1:
+        raise Exception("Ошибка! Функция принимает только целые положительные числа")
+
+    if n in (1, 2):
+        return 1
+
+    return fibonacci_rec(n-2) + fibonacci_rec(n-1)
 
 
 def fibonacci_iter(n: int) -> int:
@@ -16,7 +22,15 @@ def fibonacci_iter(n: int) -> int:
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
+    if type(n) != int or n < 1:
+        raise Exception("Ошибка! Функция принимает только целые положительные числа")
+
+    first = second = 1
+    for _ in range(3, n+1):
+        first, second = second, first + second
+
+    return second
+
 
 
 def rabbits(month: int, lifetime: int) -> int:
@@ -30,6 +44,18 @@ def rabbits(month: int, lifetime: int) -> int:
     :return: количество пар кроликов
     """
     pass
+    # if month < 2:
+    #     return 1
+    #
+    # previousMonth = currentMonth = 1
+    # for i in range(2, month+1):
+    #     previousMonth, currentMonth = currentMonth, previousMonth + currentMonth
+    #
+    #     if i >= lifetime:
+    #         currentMonth -= previousMonth
+    #
+    # return currentMonth
+
 
 
 def main():
@@ -52,4 +78,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    print(rabbits(4, 8))
