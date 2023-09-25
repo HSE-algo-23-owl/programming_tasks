d9d8bab4a83258808e3f0bfbab64eb50a3dd9ba5
Aron Shamatava <agshamatava@mail.ru>
2023-09-25 18:26:42 +0500

    фибоначчи рекурсивно и итерационно

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import time
 
 
 def fibonacci_rec(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована рекурсивно.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
+    fib1 = fib2 = 1
+    fibres = int
+    if (n == 1 or n == 2):
+        return 1
+    elif (n > 2):
+        fibres = fibonacci_rec(n - 1) + fibonacci_rec(n - 2)
+    return fibres
 
 
 def fibonacci_iter(n: int) -> int:
-    """Возвращает N-е число Фибоначчи. Реализована итеративно.
-
-    :param n: порядковый номер числа Фибоначчи
-    :return: число Фибоначчи
-    """
-    pass
+    fib1 = fib2 = 1
+    if (n < 3):
+        return 1
+    for i in range(2, n):
+        fib1, fib2 = fib2, fib1 + fib2
+    return fib2
 
 
 def rabbits(month: int, lifetime: int) -> int:
@@ -29,11 +30,17 @@ def rabbits(month: int, lifetime: int) -> int:
     :param lifetime: продолжительность жизни кролика, не менее 2 месяцев
     :return: количество пар кроликов
     """
-    pass
+    """if (lifetime < 3):
+        return 1
+    elif (lifetime < month):
+        diff = month - lifetime
+        return (2**month - 2**(diff - 1))
+    elif (lifetime >= month):
+        return 2**month"""
 
 
 def main():
-    n = 35
+    n = 10
     print(f'Вычисление {n} числа Фибоначчи рекурсивно:')
     start_time = time.time()
     print(fibonacci_rec(n))
@@ -44,11 +51,11 @@ def main():
     print(fibonacci_iter(n))
     print(f'duration: {time.time() - start_time} seconds')
 
-    if rabbits(1, 2):
-        lifetime = 5
+    """if rabbits(1, 2):
+        lifetime = 3
         print(f'\nВычисление числа пар кроликов по состоянию на {n} месяц')
         print(f'при продолжительности жизни кролика {lifetime} месяцев')
-        print(rabbits(n, lifetime))
+        print(rabbits(n, lifetime))"""
 
 
 if __name__ == '__main__':
