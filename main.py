445ea460ecdefda8569fc3fda122f9ec84c920e3
egorb <egorbobylev398@gmail.com>
2023-09-25 16:12:53 +0500

    All functions for a task implemented (fibonacci_rec, fibonacci_iter, rabbits)

--- main.py
+++ main.py
@@ -2,24 +2,40 @@ import time
 
 
 def fibonacci_rec(n: int) -> int:
+    if n == 1 or n == 2:
+        return 1
+    return fibonacci_rec(n-1) + fibonacci_rec(n-2)
     """Возвращает N-е число Фибоначчи. Реализована рекурсивно.
-
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
 
 
 def fibonacci_iter(n: int) -> int:
+    fib1 = 1
+    fib2 = 1
+    if n == 1 or n == 2:
+        return 1
+    for i in range (2, n):
+        fib1, fib2 = fib2, fib2 + fib1
+    return fib2
     """Возвращает N-е число Фибоначчи. Реализована итеративно.
 
     :param n: порядковый номер числа Фибоначчи
     :return: число Фибоначчи
     """
-    pass
 
 
 def rabbits(month: int, lifetime: int) -> int:
+    population = [1, 1]
+    if month == 1 or month == 2:
+        return 1
+    for i in range (2, month):
+        term = population[i - 1] + population[i - 2]
+        if i >= lifetime:
+            term -= population[i - lifetime]
+        population.append(term)
+    return population[month - 1]
     """Возвращает количество пар кроликов в популяции на заданный месяц.
     В начальный момент времени имеется одна пара кроликов. Начиная со второго
     месяца после рождения кролики производят новую пару кроликов каждый месяц.
@@ -29,7 +45,6 @@ def rabbits(month: int, lifetime: int) -> int:
     :param lifetime: продолжительность жизни кролика, не менее 2 месяцев
     :return: количество пар кроликов
     """
-    pass
 
 
 def main():
