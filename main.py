7f64cfeafc3e48ca60f22d8c3ebd2f86165e4322
Victor Voronov <vav0701@mail.ru>
2023-09-26 14:25:31 +0500

    determinant by 1.KLA$ team

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
-def calculate_determinant(matrix: [[int]]) -> int:
-    """Вычисляет определитель целочисленной квадратной матрицы
+def is_suitable_matrix(matrix):
+    if type(matrix) != list or len(matrix) == 0:
+        raise Exception('Вы ввели не матрицу')
+    matrix_dimension = len(matrix)
+    for row in matrix:
+        if type(row) != list:
+            raise Exception('Ваша матрица содержит разные типы данных')
+        if len(row) != matrix_dimension:
+            raise Exception('Вы ввели не квадратную матрицу')
+        if None in row:
+            raise Exception('У вас None в строке))')
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
-    квадратной матрицей
-    :return: значение определителя
-    """
-    pass
+def calculate_minor(matrix, i, j):
+    tmp = []
+    bb = []
+    for k in range(len(matrix)):
+        tmp.clear()
+        for s in range(len(matrix)):
+            if i != k and s != j:
+                tmp.append(matrix[k][s])
+        if len(tmp) > 0:
+            bb.append([int(x) for x in tmp])
+    return bb
+
+
+def calculate_determinant(matrix: [[int]]) -> int:
+    """Вычисляет определитель целочисленной квадратной матрицы"""
+    is_suitable_matrix(matrix)
+    ans = 0
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    for i in range(len(matrix)):
+        ans += (-1) ** i * matrix[i][0] * calculate_determinant(calculate_minor(matrix, i, 0))
+    return ans
 
 
 def main():
-    matrix = [[1, 2],
-              [3, 4]]
+    matrix = []
     print('Матрица')
     for row in matrix:
         print(row)
