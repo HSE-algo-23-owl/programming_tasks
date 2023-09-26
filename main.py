6b3383c2bb6552edb05ad601b622d7edf60801e4
nikit <nikit@DESKTOP-7GOJSGL>
2023-09-26 23:00:47 +0500

    2_add

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
-import random
-
 import numpy as np
 import random as rnd
-
 
 MATRIX = 'matrix'
 DET = 'det'
 
 
-def get_random_matrix_and_det(order):
-    """Генерирует случайную квадратную целочисленную матрицу с заранее
-    известным значением определителя.
+def input_exception(order: int):
+    if type(order) != int:
+        raise Exception('Порядок матрицы не является целым числом')
+    if order < 1:
+        raise Exception('Порядок матрицы меньше 1')
 
-    :param order: порядок матрицы
-    :raise Exception: если порядок матрицы не является целым числом и порядок
-    меньше 1
-    :return: словарь с ключами matrix, det
-    """
-    pass
+
+def get_matrix(order: int) -> [[int]]:  # генерация матрици с определителем = 1
+    matrix = []
+    for i in range(order):
+        matrix.append([1] * order)
+    for i in range(order - 1):
+        matrix[i][i] += 1
+    return matrix
+
+
+def get_random_matrix_and_det(order: int):
+    input_exception(order)
+    det = 1
+    matrix = get_matrix(order)
+    for i in range(order):
+        rand1 = rnd.randint(1, 10)  # можно поменять значения; чем больше значение, тем более разные числа
+        rand2 = rnd.randint(1, 10)
+        for j in range(order):
+            matrix[i][j] *= rand1
+            matrix[j][i] *= rand2
+        det *= rand2 * rand1
+
+    matrix_and_det = {MATRIX: matrix, DET: det}
+    return matrix_and_det
 
 
 def main():
-    n = 10
+    n = 10.1
     print('Генерация матрицы порядка 10')
     gen_result = get_random_matrix_and_det(n)
     [print(row) for row in gen_result[MATRIX]]
