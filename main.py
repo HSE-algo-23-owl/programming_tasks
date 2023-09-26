6c8fdd7bf54e6d70958c1bb9c2ee2a8efbd140ea
EugenMusihin <jenya.musihin040@gmail.com>
2023-09-26 17:00:58 +0500

    выполнили первое задание

--- main.py
+++ main.py
@@ -6,8 +6,39 @@ def calculate_determinant(matrix: [[int]]) -> int:
     квадратной матрицей
     :return: значение определителя
     """
-    pass
+    check_matrix_exception(matrix)
+    if (len(matrix) == 1):
+        return matrix[0][0]
+    row_index = 0
+    det = 0
+    for col_index in range(len(matrix)):
+        if (matrix[row_index][col_index] == 0):
+            continue
+        det += (matrix[row_index][col_index] * (-1)**(row_index+col_index) *
+                get_minor(matrix, row_index, col_index))
+    return det
 
+
+def check_matrix_exception(matrix):
+    if type(matrix) != list:
+        raise Exception('Ошибка, задана не матрица')
+    if matrix == []:
+        raise Exception('Ошибка, пустая матрица')
+
+    for row in matrix:
+        if len(matrix) != len(row):
+            raise Exception('Ошибка, матрица не квадратная')
+def get_minor(matrix, row_index, col_index):
+    reduced_matrix = []
+    for cur_row_idx, row in enumerate(matrix):
+        if cur_row_idx != row_index:
+            new_row = []
+            for cur_col_idx, col in enumerate(row):
+                if cur_col_idx != col_index:
+                    new_row.append(col)
+            reduced_matrix.append(new_row)
+    determinant = calculate_determinant(reduced_matrix)
+    return determinant
 
 def main():
     matrix = [[1, 2],
