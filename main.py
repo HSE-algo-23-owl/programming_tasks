7eec0ef2c7b193bd13e0286248a7de05b62c2739
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2023-10-02 19:30:47 +0500

    Добавил задание 3

--- main.py
+++ main.py
@@ -1,22 +1,79 @@
-def calculate_determinant(matrix: [[int]]) -> int:
-    """Вычисляет определитель целочисленной квадратной матрицы
+import time
 
-    :param matrix: целочисленная квадратная матрица
-    :raise Exception: если значение параметра не является целочисленной
-    квадратной матрицей
-    :return: значение определителя
+
+def gcd_recursive(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Рекурсивная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def gcd_iterative_slow(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Медленная итеративная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def gcd_iterative_fast(a: int, b: int) -> int:
+    """Вычисляет наибольший общий делитель двух целых чисел.
+    Быстрая итеративная реализация
+
+    :param a: целое число a
+    :param b: целое число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    :return: значение наибольшего общего делителя
+    """
+    pass
+
+
+def lcm(a: int, b: int) -> int:
+    """Вычисляет наименьшее общее кратное двух натуральных чисел
+
+    :param a: натуральное число a
+    :param b: натуральное число b
+    :raise Exception: если a или b не являются натуральными числами или
+    они равны нулю
+    :return: значение наименьшего общего кратного
     """
     pass
 
 
 def main():
-    matrix = [[1, 2],
-              [3, 4]]
-    print('Матрица')
-    for row in matrix:
-        print(row)
+    a = 1005002
+    b = 1354
+    print(f'Вычисление НОД чисел {a} и {b} рекурсивно:')
+    start_time = time.time()
+    print(gcd_recursive(a, b))
+    print(f'Продолжительность: {time.time() - start_time} сек')
 
-    print(f'Определитель матрицы равен {calculate_determinant(matrix)}')
+    print(f'\nВычисление НОД чисел {a} и {b} итеративно с вычитанием:')
+    start_time = time.time()
+    print(gcd_iterative_slow(a, b))
+    print(f'Продолжительность: {time.time() - start_time} сек')
+
+    print(f'\nВычисление НОД чисел {a} и {b} итеративно с делением:')
+    start_time = time.time()
+    print(gcd_iterative_fast(a, b))
+    print(f'Продолжительность: {time.time() - start_time} сек')
+
+    print(f'\nВычисление НОК чисел {a} и {b}:')
+    start_time = time.time()
+    print(lcm(a, b))
+    print(f'Продолжительность: {time.time() - start_time} сек')
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,63 +1,100 @@
 import unittest
 
-from main import calculate_determinant
+from main import gcd_recursive, gcd_iterative_slow, gcd_iterative_fast, lcm
 
 
-class TestDeterminant(unittest.TestCase):
-    """Набор тестов для проверки функции вычисления определителя
-    целочисленной квадратной матрицы"""
-    def test_none(self):
-        """Проверяет, что функция выбрасывает исключение при передаче в
-        параметр значения None"""
-        self.assertRaises(Exception, calculate_determinant, None)
+A = 0
+"""Индекс числа a в тестовых наборах данных"""
+B = 1
+"""Индекс числа b в тестовых наборах данных"""
+RESULT = 2
+"""Индекс результата в тестовых наборах данных"""
 
-    def test_empty_matrix(self):
-        """Проверяет, что функция выбрасывает исключение при передаче в
-        параметр пустого списка"""
-        self.assertRaises(Exception, calculate_determinant, [])
 
-    def test_first_order(self):
-        """Проверяет расчет определителя для матрицы порядка 1"""
-        matrix = [[1]]
-        self.assertEqual(calculate_determinant(matrix), 1)
+class TestGcd(unittest.TestCase):
+    """Набор тестов для проверки реализации функций расчета НОД.
+    Каждый тест запускается трижды, для каждой из реализаций расчета НОД"""
+    gcd_functions = (gcd_recursive, gcd_iterative_slow, gcd_iterative_fast)
+    incorrect_inputs = (None, 'string', 1.1, [])
+    simple_set = ((9, 3, 3),
+                  (-9, 3, 3),
+                  (3, 5, 1),
+                  (-18, -45, 9),
+                  (1, 0, 1),
+                  (0, 1, 1))
+    long_set = ((1005002, 1354, 2),
+                (-1005002, 1354, 2),
+                (65225655, 1610510, 805255),
+                (-1944, -332024, 8),
+                (65225655, 0, 65225655),
+                (0, 65225655, 65225655))
 
-    def test_second_order(self):
-        """Проверяет расчет определителя для матрицы порядка 2"""
-        matrix = [[1, 2],
-                  [3, 4]]
-        self.assertEqual(calculate_determinant(matrix), -2)
+    def test_incorrect_first(self):
+        """Проверка выброса исключения при передаче некорректного значения
+        в параметр a"""
+        err_message = 'Значение параметра a не является целым числом'
+        for gcd in self.gcd_functions:
+            for val in self.incorrect_inputs:
+                self.assertRaisesRegex(Exception, err_message, gcd, val, 1)
 
-    def test_third_order(self):
-        """Проверяет расчет определителя для матрицы порядка 3"""
-        matrix = [[1, -2, 3],
-                  [-4, 5, -6],
-                  [7, -8, 9]]
-        self.assertEqual(calculate_determinant(matrix), 0)
+    def test_incorrect_second(self):
+        """Проверка выброса исключения при передаче некорректного значения
+        в параметр b"""
+        err_message = 'Значение параметра b не является целым числом'
+        for gcd in self.gcd_functions:
+            for val in self.incorrect_inputs:
+                self.assertRaisesRegex(Exception, err_message, gcd, 1, val)
 
-    def test_fourth_order(self):
-        """Проверяет расчет определителя для матрицы порядка 4"""
-        matrix = [[3, -3, -5, 8],
-                  [-3, 2, 4, -6],
-                  [2, -5, -7, 5],
-                  [-4, 3, 5, -6]]
-        self.assertEqual(calculate_determinant(matrix), 18)
+    def test_both_zero(self):
+        """Проверка выброса исключения при передаче 0 в параметры a и b"""
+        err_message = 'Значения параметров a и b равны нулю'
+        for gcd in self.gcd_functions:
+            self.assertRaisesRegex(Exception, err_message, gcd, 0, 0)
 
-    def test_not_square_rectangle(self):
-        """Проверяет, что функция выбрасывает исключение при передаче в
-        параметр прямоугольной матрицы"""
-        matrix = [[3, -3, -5, 8],
-                  [-3, 2, 4, -6],
-                  [-4, 3, 5, -6]]
-        self.assertRaises(Exception, calculate_determinant, matrix)
+    def test_simple(self):
+        """Проверка вычисления НОД на примерах небольших чисел"""
+        for gcd in self.gcd_functions:
+            for row in self.simple_set:
+                self.assertEqual(row[RESULT], gcd(row[A], row[B]))
 
-    def test_not_square_jag(self):
-        """Проверяет, что функция выбрасывает исключение при передаче в
-        параметр матрицы с различной длиной строк"""
-        matrix = [[3, -3, -5, 8],
-                  [-3, 2, 4, -6],
-                  [2, -5, -7],
-                  [-4, 3, 5, -6]]
-        self.assertRaises(Exception, calculate_determinant, matrix)
+    def test_long(self):
+        """Проверка вычисления НОД на примерах относительно больших чисел"""
+        for gcd in self.gcd_functions:
+            for row in self.long_set:
+                self.assertEqual(row[RESULT], gcd(row[A], row[B]))
+
+
+class TestLcm(unittest.TestCase):
+    """Набор тестов для проверки реализации функции расчета НОК"""
+    incorrect_inputs = (None, 'string', 1.1, [], 0, -1)
+
+    def test_incorrect_first(self):
+        """Проверка выброса исключения при передаче некорректного значения
+        в параметр a"""
+        err_message = ('Значение параметра a не является натуральным '
+                       'положительным числом')
+        for val in self.incorrect_inputs:
+            self.assertRaisesRegex(Exception, err_message, lcm, val, 1)
+
+    def test_incorrect_second(self):
+        """Проверка выброса исключения при передаче некорректного значения
+        в параметр b"""
+        err_message = ('Значение параметра b не является натуральным '
+                       'положительным числом')
+        for val in self.incorrect_inputs:
+            self.assertRaisesRegex(Exception, err_message, lcm, 1, val)
+
+    def test_primes(self):
+        """Проверка вычисления НОК для простых чисел"""
+        self.assertEqual(21, lcm(7, 3))
+
+    def test_simple(self):
+        """Проверка вычисления НОК для небольших чисел"""
+        self.assertEqual(24, lcm(6, 8))
+
+    def test_simple_long(self):
+        """Проверка вычисления НОК для относительно больших чисел"""
+        self.assertEqual(680386354, lcm(1005002, 1354))
 
 
 if __name__ == '__main__':
