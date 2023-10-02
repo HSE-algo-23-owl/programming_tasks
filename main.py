b94279257a64d23a9e1d1e379c17e9b6d488587e
Alena Anuchina <anuchina05@gmail.com>
2023-10-02 23:01:15 +0500

    Убрала закоментированный код

--- main.py
+++ main.py
@@ -16,11 +16,7 @@ def calculate_determinant(matrix: [[int]]) -> int:
     return det
 
 
-'''def calculate_minor(matrix):
-    if len(matrix) == 1:
-        return matrix[0][0]
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]'''
+
 
 
 
