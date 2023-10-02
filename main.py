6e45abd396dfef556d363c0740d4fef869c80145
Aron Shamatava <agshamatava@mail.ru>
2023-10-02 21:03:34 +0500

    подправил проверку матрицы

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def check_matrix(matrix):
+    if type(matrix) != list:
+        raise Exception("Error, this non-list type.")
+    length = len(matrix)
+    if length == 0:
+        raise Exception("Error, empty matrix.")
+    for row in range(len(matrix)):
+        if type(matrix[row]) != list:
+            raise Exception("Error, this non-list type.")
+        elif len(matrix[row]) != length:
+            raise Exception("Error, this is not a square matrix.")
+
+
 def submatrix(matrix, row, col):
     # Helper function to get the submatrix of a matrix by removing a specific row and column
     return [row[:col] + row[col + 1:] for row in (matrix[:row] + matrix[row + 1:])]
@@ -16,7 +29,7 @@ def calculate_determinant(matrix: [[int]]) -> int:
 
         # Initialize the determinant
     det = 0
-
+    check_matrix(matrix)
     # Iterate through the first row to calculate the determinant
     for col in range(len(matrix[0])):
         # Calculate the cofactor for the current element
