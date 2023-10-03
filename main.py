0042f4b3461ee3532f97b8d979ec53e25bdb3298
Victor <vav0701@mail.ru>
2023-10-03 14:22:33 +0500

    lcm and gdc methods by 1.KLA$ team

--- main.py
+++ main.py
@@ -1,55 +1,77 @@
 import time
 
+def __is_suitable_gcd_values(n1, n2):
+    if type(n1) != int:
+        raise Exception('Значение параметра a не является целым числом')
+    if type(n2) != int:
+        raise Exception('Значение параметра b не является целым числом')
+    if n1 == 0 and n2 == 0:
+        raise Exception('Значения параметров a и b равны нулю')
 
-def gcd_recursive(a: int, b: int) -> int:
+
+def __is_suitable_lcm_values(n1, n2):
+    if type(n1) != int or n1 < 1:
+        raise Exception('Значение параметра a не является натуральным '
+                       'положительным числом')
+    if type(n2) != int or n2 < 1:
+        raise Exception('Значение параметра b не является натуральным '
+                       'положительным числом')
+
+
+def gcd_recursive(n1: int, n2: int) -> int:
     """Вычисляет наибольший общий делитель двух целых чисел.
     Рекурсивная реализация
-
-    :param a: целое число a
-    :param b: целое число b
-    :raise Exception: если a или b не являются целыми числами или
-    они оба равны нулю
-    :return: значение наибольшего общего делителя
     """
-    pass
+    __is_suitable_gcd_values(n1, n2)
+    n1 = abs(n1)
+    n2 = abs(n2)
+    if n1 == 0:
+        return n2
+    elif n2 == 0:
+        return n1
+    else:
+        return gcd_recursive(n2, n1 % n2)
 
 
-def gcd_iterative_slow(a: int, b: int) -> int:
+def gcd_iterative_slow(n1: int, n2: int) -> int:
     """Вычисляет наибольший общий делитель двух целых чисел.
     Медленная итеративная реализация
-
-    :param a: целое число a
-    :param b: целое число b
-    :raise Exception: если a или b не являются целыми числами или
-    они оба равны нулю
-    :return: значение наибольшего общего делителя
     """
-    pass
+    __is_suitable_gcd_values(n1, n2)
+    n1 = abs(n1)
+    n2 = abs(n2)
+    while True:
+        if n1 == 0 or n2 == 0:
+            return max(n1, n2)
+        if n1 >= n2:
+            n1 -= n2
+        else:
+            n2 -= n1
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
     """Вычисляет наибольший общий делитель двух целых чисел.
     Быстрая итеративная реализация
-
-    :param a: целое число a
-    :param b: целое число b
-    :raise Exception: если a или b не являются целыми числами или
-    они оба равны нулю
-    :return: значение наибольшего общего делителя
     """
-    pass
+    __is_suitable_gcd_values(a, b)
+    a = abs(a)
+    b = abs(b)
+    while True:
+        if a == 0 or b == 0:
+            return max(a, b)
+        if a > b:
+            a %= b
+        else:
+            b %= a
 
 
-def lcm(a: int, b: int) -> int:
+def lcm(n1: int, n2: int) -> int:
     """Вычисляет наименьшее общее кратное двух натуральных чисел
-
-    :param a: натуральное число a
-    :param b: натуральное число b
-    :raise Exception: если a или b не являются натуральными числами или
-    они равны нулю
-    :return: значение наименьшего общего кратного
     """
-    pass
+    __is_suitable_lcm_values(n1, n2)
+    n1 = abs(n1)
+    n2 = abs(n2)
+    return int((n1 * n2) / gcd_iterative_fast(n1, n2))
 
 
 def main():
