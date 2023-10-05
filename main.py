d593955f1ee564514d6b86dc4f9f9fe6d6f77e7a
Timofei <tscherepanov@edu.hse.ru>
2023-10-05 16:50:35 +0500

    Task 3 completed

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import time
 
+def __validate_data(a: int, b: int) -> None:
+    """Вспомогательная функция для валидации входящих параметров
+    :param a: целое число a
+    :param b: целое число b
+    :raise Exception: если a или b не являются целыми числами или
+    они оба равны нулю
+    """
+    if type(a) != int:
+        raise Exception('Значение параметра a не является целым числом')
+    if type(b) != int:
+        raise Exception('Значение параметра b не является целым числом')
+    if a == b == 0:
+        raise Exception('Значения параметров a и b равны нулю')
+
 
 def gcd_recursive(a: int, b: int) -> int:
     """Вычисляет наибольший общий делитель двух целых чисел.
@@ -11,7 +25,17 @@ def gcd_recursive(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    __validate_data(a, b)
+    a, b = abs(a), abs(b)  # если на входе отрицательные числа
+
+    if a == b:  # если числа равны, возвращаем любое из этих чисел
+        return a
+    if a * b == 0:  # если одно из чисел равно нулю, возвращаем максимальное (сумму)
+        return a + b
+    if b > a:  # меняем числа местами, если второе число больше первого
+        a, b = b, a
+
+    return gcd_recursive(a - b, b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
@@ -24,7 +48,17 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    __validate_data(a, b)
+    a, b = map(abs, (a, b))
+
+    if a * b == 0:
+        return a + b
+
+    while a != b:
+        a, b = max(a, b), min(a, b)
+        a -= b
+
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
@@ -37,7 +71,13 @@ def gcd_iterative_fast(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    __validate_data(a, b)
+    a, b = map(abs, (a, b))
+
+    while b:
+        a, b = b, a % b
+
+    return a
 
 
 def lcm(a: int, b: int) -> int:
@@ -49,7 +89,17 @@ def lcm(a: int, b: int) -> int:
     они равны нулю
     :return: значение наименьшего общего кратного
     """
-    pass
+
+    # валидиация данных при помощи генерации словаря и перебора параметров функции - костыль, гораздо логичнее было
+    # воспользоваться и пробежаться по коллекции (кортежу) позиционных аргументов args, но тогда бы пришлось менять
+    # сигнатуру функции, к тому же была бы нарушена логика задания (функция может принимать только два аргумента)
+    params = {param: value for param, value in locals().items()}  # формируем словарь из параметров функции
+    for param in params.keys():
+        if type(params[param]) != int or params[param] < 1:  # если хотя бы один параметр не
+            # является целочисленным числом и меньше единицы
+            raise Exception(f"Значение параметра {param} не является натуральным положительным числом")
+
+    return int(a * b / gcd_iterative_fast(a, b))
 
 
 def main():
@@ -78,3 +128,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
