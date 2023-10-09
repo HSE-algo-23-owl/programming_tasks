2049f20f00f7d0dfd4f2f89fef5839b0667623ce
Aron Shamatava <agshamatava@mail.ru>
2023-10-09 22:07:54 +0500

    сделали все обязательные методы 3 задания

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import time
 
 
+def check_gcd(a, b):
+    if type(a) != int:
+        raise Exception("Значение параметра a не является целым числом")
+    if type(b) != int:
+        raise Exception("Значение параметра b не является целым числом")
+    if a == b == 0:
+        raise Exception("Значения параметров a и b равны нулю.")
+
+
+def check_lcm(a, b):
+    if type(a) != int or a <= 0:
+        raise Exception("Значение параметра a не является натуральным положительным числом")
+    if type(b) != int or b <= 0:
+        raise Exception("Значение параметра b не является натуральным положительным числом")
+
+
 def gcd_recursive(a: int, b: int) -> int:
     """Вычисляет наибольший общий делитель двух целых чисел.
     Рекурсивная реализация
@@ -11,7 +27,15 @@ def gcd_recursive(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    check_gcd(a, b)
+    a = abs(a)
+    b = abs(b)
+    if a * b == 0:
+        return a + b
+    if a == b :
+        return a
+    else:
+        return gcd_recursive(b, a % b)
 
 
 def gcd_iterative_slow(a: int, b: int) -> int:
@@ -24,7 +48,19 @@ def gcd_iterative_slow(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    check_gcd(a, b)
+    a = abs(a)
+    b = abs(b)
+    if a * b == 0:
+        return a + b
+    if a == b:
+        return a
+    while a != b:
+        if a > b:
+            a -= b
+        else:
+            b -= a
+    return a
 
 
 def gcd_iterative_fast(a: int, b: int) -> int:
@@ -37,7 +73,16 @@ def gcd_iterative_fast(a: int, b: int) -> int:
     они оба равны нулю
     :return: значение наибольшего общего делителя
     """
-    pass
+    check_gcd(a, b)
+    a = abs(a)
+    b = abs(b)
+    if a * b == 0:
+        return a + b
+    if a == b :
+        return a
+    while a % b != 0:
+        a, b = b, a % b
+    return b
 
 
 def lcm(a: int, b: int) -> int:
@@ -49,12 +94,15 @@ def lcm(a: int, b: int) -> int:
     они равны нулю
     :return: значение наименьшего общего кратного
     """
-    pass
+    check_lcm(a, b)
+    a = abs(a)
+    b = abs(b)
+    return (a*b)/(gcd_iterative_fast(a, b))
 
 
 def main():
-    a = 1005002
-    b = 1354
+    a = 30
+    b = 6
     print(f'Вычисление НОД чисел {a} и {b} рекурсивно:')
     start_time = time.time()
     print(gcd_recursive(a, b))
