13d4082c3c39c6bc43e7579165b7e24337c886fb
Timofei <tscherepanov@edu.hse.ru>
2023-10-14 01:46:54 +0500

    Realized iterative function of generation permutations

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def generate_permutations(items: frozenset[Any]) -> list[list[Any]]:
     __validate(items)
     items_set = set(item for item in items)
 
-    return sorted(__generate_permutations_rec(items_set))
+    return __generate_permutations_iter(items_set)
 
 
 def __generate_permutations_rec(items_set: set[Any]) -> list[list[Any]]:
@@ -41,6 +41,35 @@ def __generate_permutations_rec(items_set: set[Any]) -> list[list[Any]]:
     return pmt_lst_help
 
 
+def __generate_permutations_iter(items_set: set[Any]) -> list[list[Any]]:
+    """Вспомогательная функция для генерации перестановок элементов итеративным методом
+    :param items_set: изменяемое множество элементов
+    :return: список перестановок, где каждая перестановка список элементов
+    множества
+    """
+    if len(items_set) < 1:
+        return []
+    if len(items_set) == 1:
+        return [[items_set.pop()]]
+
+    permutations_list = [[items_set.pop()]]  # список со всеми перестановками
+
+    while items_set:  # пока множество содержит элементы для перестановок
+        permutations_iteration_list = []  # список, содержащий перестановки всех элементов в одной итерации
+        current_item = items_set.pop()  # текущий элемент, с которым будут генерироваться перестановки на итерации
+        for permutation in permutations_list:  # обход вложенных списков перестановок с добавлением нового элемента
+            permutation.append(current_item)
+
+            permutations_iteration_list.append(permutation)  # добавление главной перестановки в итерационный список
+            for pos in range(len(permutation)-1):  # переставление нового элемента местами на различные позиции
+                pmt_lst = [item for item in permutation]  # создание списка для перестановки элементов
+                pmt_lst[-1], pmt_lst[pos] = pmt_lst[pos], pmt_lst[-1]  # перестановка двух элементов местами
+                permutations_iteration_list.append(pmt_lst)  # добавление получившийся перестановки во временный список
+        permutations_list = permutations_iteration_list  # добавление всех итерационных перестановок в общий список
+
+    return sorted(permutations_list)
+
+
 def __validate(items: Any) -> None:
     """Вспомогательная функция для валидации данных
     :param items: любой тип данных
@@ -57,3 +86,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
