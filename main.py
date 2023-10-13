3f9d723967d7e566f772deb416daecd558cb1ffe
Victor <vav0701@mail.ru>
2023-10-13 21:23:30 +0500

    permutations with O(N) recursion by 1.KLA$ team

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 from typing import Any
 
 
-def __is_suitable_values(elements):
-    if type(elements) != frozenset:
+def __is_suitable_values(items):
+    if type(items) != frozenset:
         raise TypeError('Параметр items не является неизменяемым множеством')
 
 
 def generate_permutations(elements: frozenset[Any]) -> list[Any]:
     """Генерирует все варианты перестановок элементов указанного множества"""
     __is_suitable_values(elements)
+    if len(elements) == 0:
+        return []
+    return __permutations([x for x in elements])
+
+
+def __permutations(elements):
     if len(elements) == 1:
         return [list(elements)]
+    cur = elements.pop()
     res = []
-    for elem in elements:
-        tmp = generate_permutations(frozenset([x for x in elements if x != elem]))
-        for x in tmp:
-            res.append([elem] + list(x))
+    for elem in __permutations(elements):
+        res.append(elem)
+        elem.append(cur)
+        for j in range(len(elem) - 1):
+            swap = [x for x in elem]
+            tmp = swap[-1]
+            swap[-1] = swap[j]
+            swap[j] = tmp
+            res.append(swap)
     return res
 
 
 def main():
-    items = frozenset([1, 2, 3])
-    print(generate_permutations(items))
+    print(generate_permutations(frozenset([1, 2])))
 
 
 if __name__ == '__main__':
