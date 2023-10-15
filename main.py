a2963d0cfa0db3236a53043ffa5c98857fa465af
nikit <nikit@DESKTOP-7GOJSGL>
2023-10-15 18:52:41 +0500

    task 4 is solved

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 from typing import Any
 
 
+def validate_list(items: frozenset[Any]):
+    if type(items) != frozenset:
+        raise TypeError('Параметр items не является неизменяемым множеством')
+
+
+def swap(items, i: int, j: int):
+    temp = items[i]
+    items[i] = items[j]
+    items[j] = temp
+
+
+def generate_permutations_logic(items: list[Any], n: int, result: list[Any]) -> list[Any]:
+    if n == 1:
+        result.append(items.copy())
+    else:
+        for i in range(n):
+            generate_permutations_logic(items, n - 1, result)
+            if n % 2 == 0:
+                swap(items, i, n - 1)
+            else:
+                swap(items, 0, n - 1)
+    return result
+
+
 def generate_permutations(items: frozenset[Any]) -> list[Any]:
-    """Генерирует все варианты перестановок элементов указанного множества
-    :param items: неизменяемое множество элементов
-    :raise TypeError: если параметр items не является неизменяемым множеством
-    :return: список перестановок, где каждая перестановка список элементов
-    множества
-    """
-    pass
+    validate_list(items)
+    items_list = list(items)
+    result = []
+    result = generate_permutations_logic(items_list, len(items), result)
+    return result
 
 
 def main():
