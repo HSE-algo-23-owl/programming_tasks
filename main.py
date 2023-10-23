7b3972ac910f1899be5e4b85997f7fb4dad288bd
Aron Shamatava <agshamatava@mail.ru>
2023-10-23 21:11:45 +0500

    сделали

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
 from typing import Any
 
 
-def validate_list(items: frozenset[Any]):
+def check_gen_rec(items):
     if type(items) != frozenset:
-        raise TypeError('Параметр items не является неизменяемым множеством')
-
-
-def swap(items, i: int, j: int):
-    temp = items[i]
-    items[i] = items[j]
-    items[j] = temp
-
-
-def generate_permutations_logic(items: list[Any]) -> list[Any]:
-    result = []
-    n = len(items)
-    if n == 0:
-        return result
-    indexes = n * [0]
-    result.append(items.copy())
-    i = 0
-    while i < n:
-        if indexes[i] < i:
-            if i % 2 == 0:
-                swap(items, 0, i)
-            else:
-                swap(items, indexes[i], i)
-            result.append(items.copy())
-            indexes[i] += 1
-            i = 0
-        else:
-            indexes[i] = 0
-            i += 1
-    return result
+        raise TypeError("Параметр items не является неизменяемым множеством")
 
 
 def generate_permutations(items: frozenset[Any]) -> list[Any]:
-    validate_list(items)
-    items_list = list(items)
-    return generate_permutations_logic(items_list)
+    """Генерирует все варианты перестановок элементов указанного множества
+    :param items: неизменяемое множество элементов
+    :raise TypeError: если параметр items не является неизменяемым множеством
+    :return: список перестановок, где каждая перестановка список элементов
+    множества
+    """
+    check_gen_rec(items)
+    mylist = list(items)
+    # for perm in helper(mylist): ## выводит в столбик
+    #     print(perm)
+    return helper(mylist)
+
+
+def helper(lst):
+    if len(lst) == 0:
+        return []
+    elif len(lst) == 1:
+        return [lst]
+    else:
+        l = []
+        for i in range(len(lst)):
+            cur_elem = lst[i]
+            rest = lst[:i] + lst[i + 1:]
+            for perm in helper(rest):
+                l.append([cur_elem] + perm)
+        return l
 
 
 def main():
-    items = frozenset([1, 2])
+    items = frozenset([1, 2, 3])
     print(generate_permutations(items))
 
 
 if __name__ == '__main__':
     main()
+
