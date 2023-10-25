8c629941214c9d8b047b5a8ef53d088bee8f8cf1
Aron Shamatava <agshamatava@mail.ru>
2023-10-25 14:16:34 +0500

    переделали 4 задание, тесты проходит

--- main.py
+++ main.py
@@ -14,22 +14,25 @@ def generate_permutations(items: frozenset[Any]) -> list[Any]:
     множества
     """
     check(items)
+    if len(items) == 0: return []
     mylist = list(items)
-    return helper(mylist, len(items), 0)
+    result = []
+    return helper(mylist, len(items), 0, result)
 
 
-def helper(elements, n, i):
+def helper(elements, n, i, result):
     if i == n:
-        print(elements)
+        result.append(list(elements))
     for j in range(i, n):
         elements[i], elements[j] = elements[j], elements[i]
-        helper(elements, n, i + 1)
+        helper(elements, n, i + 1, result)
         elements[i], elements[j] = elements[j], elements[i]
+    return result
 
 
 def main():
     items = frozenset([1, 2, 3])
-    generate_permutations(items)
+    print(generate_permutations(items))
 
 
 if __name__ == '__main__':
