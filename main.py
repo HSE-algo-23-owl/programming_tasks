9f707d59d848fe57f05ebea6e13548234252af2a
Евгения Федурина <fedurin13@outlook.com>
2023-11-06 21:35:41 +0500

    4таск

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from typing import Any
 
 
+def check(items: frozenset[Any]):
+    if type(items) != frozenset:
+        raise TypeError('Параметр items не является неизменяемым множеством')
+
 def generate_permutations(items: frozenset[Any]) -> list[Any]:
     """Генерирует все варианты перестановок элементов указанного множества
     :param items: неизменяемое множество элементов
@@ -8,7 +12,21 @@ def generate_permutations(items: frozenset[Any]) -> list[Any]:
     :return: список перестановок, где каждая перестановка список элементов
     множества
     """
-    pass
+    check(items)
+    return get_permutation(list(items))
+
+def get_permutation(items: frozenset[Any] ):
+    if (len(items) == 0):
+        return []
+    result = list(items)
+    if len(items) == 1:
+        return [items]
+    result = []
+    for i in range(len(items)):
+        rest = items[:i] + items[i + 1:]
+        for p in get_permutation(rest):
+            result.append([items[i]] + p)
+    return result
 
 
 def main():
