11a1fca9333b029c579941be58b5cdc93ed92428
kvakita <bajdinaekaterina3@gmail.com>
2023-11-06 18:58:12 +0500

    Котятки сделаои 4 задание

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from typing import Any
 
 
-def generate_permutations(items: frozenset[Any]) -> list[Any]:
+def generate_permutations(elements: frozenset[Any]) -> list[Any]:
     """Генерирует все варианты перестановок элементов указанного множества
-    :param items: неизменяемое множество элементов
+    :param elements: неизменяемое множество элементов
     :raise TypeError: если параметр items не является неизменяемым множеством
     :return: список перестановок, где каждая перестановка список элементов
     множества
     """
-    pass
+    validate(elements)
+    return rec_permut_find(list(elements))
 
+def validate(items: frozenset[Any]):
+    if type(items) != frozenset:
+        raise TypeError("Параметр items не является неизменяемым множеством")
+
+def rec_permut_find(elements: list[Any]):
+    if len(elements) == 1:
+        return [elements]
+    result = []
+    for i in range(len(elements)):
+        current = elements[i]
+        leftover = elements[:i] + elements[i + 1:]
+        for permutation in rec_permut_find(leftover):
+            result.append([current] + permutation)
+    return result
 
 def main():
     items = frozenset([1, 2, 3])
