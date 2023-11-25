af782445d07bf60c5504397a2a83043b3396b7bc
jenya <egmusikhin@edu.hse.ru>
2023-11-25 22:33:28 +0500

    сделали 7 задание со строками

--- main.py
+++ main.py
@@ -19,7 +19,28 @@ def generate_strings(length: int) -> list[str]:
     числом.
     :return: Список строк.
     """
-    pass
+    if type(length) != int:
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    if length <= 0:
+        raise ValueError(STR_LENGTH_ERROR_MSG)
+    strings = []
+    generate_zero(length, "", strings)
+    generate_one(length, "", strings)
+    return strings
+
+def generate_zero (n:int, s:str, strings:list):
+    if len(s) + 1 == n:
+        strings.append("0" + s)
+        return
+    generate_one(n, "0" + s, strings)
+
+
+def generate_one (n:int, s:str, strings:list):
+    if len(s) + 1 == n:
+        strings.append("1" + s)
+        return
+    generate_zero(n, "1" + s, strings)
+    generate_one(n, "1" + s, strings)
 
 
 def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
@@ -31,19 +52,36 @@ def binomial_coefficient(n: int, k: int, use_rec=False) -> int:
     числами или значение параметра n меньше чем k.
     :return: Значение биномиального коэффициента.
     """
-    pass
+    if type(n) != int:
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("n"))
+    if type(k) != int:
+        raise ValueError(NOT_INT_VALUE_TEMPL.format("k"))
+    if n < 0 :
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("n"))
+    if k < 0 :
+        raise ValueError(NEGATIVE_VALUE_TEMPL.format("k"))
+    if n < k:
+        raise ValueError(N_LESS_THAN_K_ERROR_MSG)
+    if (use_rec):
+         return binomial_coefficient_rec(n, k)
+    else:
+        return binomial_coefficient_iter(n, k)
 
+def binomial_coefficient_rec(n: int, k: int) -> int:
+    if k == 0 or n == k:
+        return 1
+    return binomial_coefficient_rec(n-1, k-1)  + binomial_coefficient_rec(n-1, k)
+def binomial_coefficient_iter(n: int, k: int) -> int:
+    r = 1
+    for i in range(1, k + 1):
+        r *= n - k + i
+        r //= i
+    return r
 
 def main():
-    n = 10
-    print(f'Строки длиной {n}:\n{generate_strings(n)}')
-
-    n = 30
-    k = 20
-    print(f'Биномиальный коэффициент (итеративно) при n, k ({n}, {k}) = ',
-          binomial_coefficient(n, k))
-    print(f'Биномиальный коэффициент (рекурсивно) при n, k ({n}, {k}) = ',
-          binomial_coefficient(n, k, use_rec=True))
+    n = 7
+    k = 5
+    print(binomial_coefficient_iter(n, k))
 
 
 if __name__ == '__main__':
