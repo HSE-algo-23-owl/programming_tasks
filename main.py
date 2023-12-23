d4c61f96cb98889c183c8c9caa03191ee44205cf
KhomyakovaEkat <hkomyakovaekat@gmail.com>
2023-12-24 00:38:14 +0500

    khommy_task_0 тест hello world

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def hello() -> str:
-    """Возвращает приветствие Hello World!"""
-    pass
+def hello():
+    return 'Hello world!'
 
 
-def main():
-    print(hello())
 
-
-if __name__ == '__main__':
-    main()
