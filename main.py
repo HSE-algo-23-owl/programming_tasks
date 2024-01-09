b97fd655297567b91cb4bf93b598e9939fff0d7c
KhomyakovaEkat <hkomyakovaekat@gmail.com>
2024-01-10 00:03:39 +0500

    khommy_task_2_add

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import random
-
 import numpy as np
-import random as rnd
+import random
 
 
 MATRIX = 'matrix'
-DET = 'det'
+DET = 'determinant'
 
 
 def get_random_matrix_and_det(order):
@@ -17,16 +15,56 @@ def get_random_matrix_and_det(order):
     меньше 1
     :return: словарь с ключами matrix, det
     """
-    pass
+    if type(order) != int or order < 1:
+        raise Exception('Error: the order is not an integer or the order less then 1!')
 
+    determinant = 1
+    # создаю пустую квадратную матрицу
+    arr = []
+    matrix = [[0 for j in range(order)] for i in range(order)]
+    # обхожу матрицу и присваиваю рандомные значения элементам выше главной диагонали
+    for i in range(order):
+        for j in range(i + 1, order):
+            matrix[i][j] = random.randint(1, 10)
+        # Присвоение рандомного числа элементам главной диагонали
+        matrix[i][i] = random.randint(1, 10)
+    # Расчет детерминанта (перемножение элементов главной диагонали)
+    for i in range(len(matrix)):
+        determinant *= matrix[i][i]
+
+    matrix = get_change_matrix(matrix)
+
+    result = {MATRIX: matrix, DET: determinant}
+    return result
+def get_change_matrix(matrix):
+    n = len(matrix) - 1
+    r_st = random.randint(0, n)
+    r_end = random.randint(0, n)
+
+    # вычитание элементов строк
+    for i in range(n):
+        for j in range(0, len(matrix[i])):
+            matrix[i][j] = matrix[i][j] - matrix[n][j]
+
+    if r_st != r_end:
+        # перемена строк и столбцоы
+        for i in range(n):
+            matrix[r_st], matrix[r_end] = matrix[r_end], matrix[r_st]
+        for k in matrix:
+            k[r_st], k[j] = k[j], k[r_st]
+    else:
+        for i in range(n):
+            matrix[i], matrix[n - 1] = matrix[n - 1], matrix[i]
+
+    return matrix
 
 def main():
-    n = 10
-    print('Генерация матрицы порядка 10')
+    n = 6
+    print(f"Генерация матрицы порядка {n}")
     gen_result = get_random_matrix_and_det(n)
-    [print(row) for row in gen_result[MATRIX]]
-    print('\nОпределитель сгенерированной матрицы равен', gen_result[DET])
 
+    print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in gen_result[MATRIX]]))
+    print('\nОпределитель сгенерированной матрицы равен', gen_result[DET])
     print('\nОпределитель, рассчитанный numpy, равен',
           round(np.linalg.det(np.array(gen_result[MATRIX]))))
 
