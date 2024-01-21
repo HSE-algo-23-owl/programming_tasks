c67562d72c6a1cf4bf1fd1194c38250f5f9dee08
kvakita <bajdinaekaterina3@gmail.com>
2024-01-21 17:42:26 +0500

    Котятки сделали задание

--- main.py
+++ main.py
@@ -21,8 +21,62 @@ def get_min_cost_path(price_table: list[list[float | int | None]]) ->\
     path - путь, список кортежей с индексами ячеек, или None если пути
     не существует.
     """
-    pass
+    validate_params(price_table)
+    cost_table = get_cost_table(price_table)
+    if cost_table[-1][-1] != INF:
+        cheapest_path = find_cheapest_path(cost_table)
+        return {COST: float(cost_table[-1][-1]), PATH: cheapest_path[::-1]}
+    else:
+        return {COST: None, PATH: None}
 
+def validate_params(allow_table):
+    if allow_table is None:
+        raise ValueError("Таблица цен не является прямоугольной матрицей с числовыми значениями")
+    if allow_table == []:
+        raise ValueError("Таблица цен не является прямоугольной матрицей с числовыми значениями")
+    for i in range(len(allow_table)):
+        if allow_table[i] == []:
+            raise ValueError("Таблица цен не является прямоугольной матрицей с числовыми значениями")
+        if len(allow_table[0]) != len(allow_table[i]):
+            raise ValueError("Таблица цен не является прямоугольной матрицей с числовыми значениями")
+        for j in range(len(allow_table[i])):
+            if allow_table[i][j] is None or type(allow_table[i][j]) == float or type(allow_table[i][j]) == int:
+                continue
+            else:
+                raise ValueError("Таблица цен не является прямоугольной матрицей с числовыми значениями")
+
+
+
+def get_cost_table(price_table):
+    col_count= len(price_table[0]) + 1
+    row_count= len(price_table) + 1
+    cost_table= [[INF] * col_count for _ in range(row_count)]
+    cost_table[1][1] = price_table[0][0]
+    for row_id in range(1, row_count):
+        for col_id in range(1, col_count):
+            if price_table[row_id - 1][col_id - 1] is None:
+                cost_table[row_id][col_id] = INF
+                continue
+            if row_id != 1 or col_id != 1:
+                cost_up = cost_table[row_id - 1][col_id]
+                cost_left = cost_table[row_id][col_id - 1]
+                cost_table[row_id][col_id] = (price_table[row_id - 1][col_id - 1] + min(cost_up,cost_left))
+    return cost_table
+
+def find_cheapest_path(cost_table):
+
+    cur_row = len(cost_table) - 1
+    cur_col = len(cost_table[0]) - 1
+    path_back = [(cur_row - 1, cur_col - 1)]
+    while cur_row > 1 or cur_col > 1:
+        cost_up = cost_table[cur_row - 1][cur_col]
+        cost_left = cost_table[cur_row][cur_col - 1]
+        if cost_up <= cost_left:
+            cur_row -= 1
+        else:
+            cur_col -= 1
+        path_back.append((cur_row - 1,cur_col - 1))
+    return path_back
 
 def main():
     table = [[1, 2, 2],
