a730380ca7ae6ac924c0295dde31fe7fd6b5c36b
KhomyakovaEkat <hkomyakovaekat@gmail.com>
2024-01-23 23:38:49 +0500

    khommy_task_4 4 задание

--- main.py
+++ main.py
@@ -10,32 +10,34 @@ def generate_permutations(items: frozenset[Any]) -> list[Any]:
     """
 
     validfrozenset(items)
+    return permutations(items)
 
+def permutations(items: frozenset[Any]) -> list[Any]:
     lst = list(items)
 
     if len(lst) == 0:
         return []
 
-    if len(lst) <= 1:
+    if len(lst) == 1:
         return [lst]
 
     carr_elenment = lst.pop(0)
 
-    permut_first = generate_permutations(lst)
+    permut_all_list = permutations(lst)
 
     permut_all = []
 
-    for elem in permut_first:
+    for elem in permut_all_list:
         for i in range(len(elem) + 1):
             all = elem[0:i] + [carr_elenment] + elem[i:]
             permut_all.append(all)
 
     return permut_all
 
-# Функция валидации неизменяеммого множества эдементов
+# Функция валидации неизменяемого множества элементов
 def validfrozenset (items: frozenset[Any]) -> TypeError:
     if type(items) != frozenset:
-        return TypeError("Параметр items не является неизменяемым множеством")
+       raise TypeError("Параметр items не является неизменяемым множеством")
 
 def main():
     items = frozenset((1, 2, 3))
