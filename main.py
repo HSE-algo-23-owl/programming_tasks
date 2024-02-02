18a82ebd6a77e1c078e46da6f4e1dc8569d085a7
Alena Anuchina <anuchina05@gmail.com>
2024-02-02 16:08:36 +0500

    Мы поняли как сделать, но не знаем как по другому

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from copy import deepcopy
-
-import numpy as np
-
 PROFIT = 'profit'
 DISTRIBUTION = 'distribution'
 PARAM_ERR_MSG = ('Таблица прибыли от проектов не является прямоугольной '
@@ -33,21 +29,48 @@ def get_invest_distribution(profit_matrix: list[list[int]]) -> \
     distribution - распределение инвестиций между проектами.
     """
     validate_matrix(profit_matrix)
+    rows = len(profit_matrix)  # Количество строк (уровней инвестиций)
+    cols = len(profit_matrix[0])  # Количество столбцов (проектов)
 
-def print_matrix(matrix):
-    for row in matrix:
-        print(" ".join(map(str, row)))
+    # Создаем матрицы для вычисления прибыли и распределения инвестиций
+    max_profit_matrix = [[0] * (cols + 1) for _ in range(rows + 1)]
+    investment_distribution = [[[] for _ in range(cols + 1)] for _ in range(rows + 1)]
+    for idx in range(cols + 1):
+        investment_distribution[0][idx] = [0] * idx
 
-def max_profit(mas):
+    for project_idx in range(1, cols + 1):
+        for investment_idx in range(1, rows + 1):
+            max_profit = 0
+            selected_distribution = []
+            for prev_investment_lvl in range(investment_idx + 1):
+                current_investment_lvl = investment_idx - prev_investment_lvl
+                prev_project_profit = max_profit_matrix[prev_investment_lvl][project_idx - 1]
+                if current_investment_lvl == 0:
+                    current_project_profit = 0
+                else:
+                    current_project_profit = profit_matrix[current_investment_lvl - 1][project_idx - 1]
+                if prev_project_profit + current_project_profit > max_profit:
+                    max_profit = prev_project_profit + current_project_profit
+                    previous_project_distribution = investment_distribution[prev_investment_lvl][project_idx - 1]
+                    selected_distribution = [*previous_project_distribution, current_investment_lvl]
+            max_profit_matrix[investment_idx][project_idx] = max_profit
+            investment_distribution[investment_idx][project_idx] = selected_distribution
+
+    # Возвращаем максимальную прибыль и распределение
+    return {PROFIT: find_max_profit(profit_matrix), DISTRIBUTION: investment_distribution[-1][-1]}
+
+
+def find_max_profit(mas):
+    """Рассчитывает максимально возможную прибыль
+        :param mas: Таблица с распределением прибыли от проектов в
+        зависимости от уровня инвестиций. Проекты указаны в столбцах, уровни
+        инвестиций в строках.
+        :return: Максимальная прибыль
+        """
     mas2 = [mas[i][0] for i in range(len(mas))]
-    matrix_end = []
-    for row in mas:
-        new_row = row + [0, 0, 0]
-        matrix_end.append(new_row)
-    ln = len(mas[0])
     for j in range(1, len(mas[0])):
         h = len(mas) - 1
-        while (h >= 0):
+        while h >= 0:
             help = [0 for i in range(h + 2)]
             help[h] = mas2[h]
             help[h + 1] = mas[h][j]
@@ -56,40 +79,10 @@ def max_profit(mas):
             mas2[h] = max(help)
             help = []
             h = h - 1
+    return mas2[len(mas2) - 1]
 
-        for i in range(len(mas2)):
-            matrix_end[i][ln] = mas2[i]
-        ln = ln + 1
-    return matrix_end
-def get_distribution(matrix_end,profit_max):
-    max_profit = matrix_end[-1][-1]
-    distribution = [0] * len(profit_max[0])
-    for i in range(len(distribution)-1,-1,-1):
-        if i != 0 or i != 1:
-            predpost= len(matrix_end[0])-2
-            if max_profit == matrix_end[-1][predpost]:
-                distribution[i] = 0;
-                max_profit = matrix_end[-1][predpost]
-                break
-            predpost = len(matrix_end[0]) - 4
-            if max_profit == matrix_end[-1][predpost]:
-                distribution[i] = len(profit_max);
-                max_profit = matrix_end[-1][predpost]
-            for row in range(len(matrix_end)):
-                if max_profit == matrix_end[row][predpost]:
-                    distribution[i] = 0;
-                    max_profit = matrix_end[row][predpost]
-                break
-        if i != 0 or i!=1:
-            predpost = len(matrix_end[0]) - 3
-            for row in range(len(matrix_end)):
-                if max_profit == matrix_end[row][predpost]:
-                    distribution[i] = matrix_end;
-                    max_profit = matrix_end[row][predpost]
-                break
 
-def validate_matrix(profit_matrix: list[list[int]]) -> \
-        dict[str: int, str: list[int]]:
+def validate_matrix(profit_matrix: list[list[int]]):
     """Проверяет являться ли таблица прямоугольной матрицей с числовыми значениями
             :param profit_matrix: Таблица значений.
             :raise ValueError: Если таблица прибыли от проектов не является
@@ -122,9 +115,7 @@ def main():
                      [26, 28, 27, 25],
                      [34, 33, 29, 31],
                      [40, 39, 41, 37]]
-    # print(get_invest_distribution(profit_matrix))
-    print(profit_matrix)
-    print(print_matrix(max_profit(profit_matrix)))
+    print(get_invest_distribution(profit_matrix))
 
 
 if __name__ == '__main__':
