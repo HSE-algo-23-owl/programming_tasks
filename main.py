c518e96cec643d983ef8d84c6460dcb0ff14a5e0
KhomyakovaEkat <hkomyakovaekat@gmail.com>
2024-03-05 00:23:56 +0500

    khommy_task_10_table_path добавлено задание 10_2

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ PATH = 'path'
 PARAM_ERR_MSG = ('Таблица цен не является прямоугольной матрицей с '
                  'числовыми значениями')
 
-
 def get_min_cost_path(price_table: list[list[float | int]]) ->\
         dict[str: float, str: list[tuple[int, int]]]:
     """Возвращает путь минимальной стоимости в таблице из правого верхнего угла
@@ -19,8 +18,45 @@ def get_min_cost_path(price_table: list[list[float | int]]) ->\
     cost - стоимость минимального пути,
     path - путь, список кортежей с индексами ячеек.
     """
-    pass
+    validate_price_table(price_table)
+    return min_cost_path(price_table)
 
+def validate_price_table(table):
+    if table is None or table == [] or table == [[]]:
+        raise ValueError(PARAM_ERR_MSG)
+
+    rows = len(table)
+    for row in table:
+        if len(row) != len(table[0]):
+            raise ValueError(PARAM_ERR_MSG)
+
+        for elem in row:
+            if elem is None or type(elem) == str:
+                raise ValueError(PARAM_ERR_MSG)
+
+def min_cost_path (table: list[list[int]]) -> int:
+    row, col = len(table), len(table[0])
+
+    F = [[INF for j in range(col + 1)] for i in range(row + 1)]
+    # стартовый случай
+    F[1][1] = table[0][0]
+
+    for i in range(1, row + 1):
+        for j in range(1, col + 1):
+            if i == j == 1:
+                continue
+            F[i][j] = (min(F[i-1][j], F[i][j-1])) + table[i-1][j-1]
+
+    path = []
+    while i != 1 or j != 1:
+        path.append((i - 1, j - 1))
+        if F[i][j - 1] > F[i - 1][j]:
+            i -= 1
+        else:
+            j -= 1
+    path.append((0, 0))
+
+    return {COST: float(F[-1][-1]), PATH: path[::-1]}
 
 def main():
     table = [[1, 2, 2],
