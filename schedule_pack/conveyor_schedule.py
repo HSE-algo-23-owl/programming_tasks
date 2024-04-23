64f52f5b55653cf5034a24ea4ba86eaef3eaea26
Aron Shamatava <agshamatava@mail.ru>
2024-04-23 11:20:58 +0500

    сделали 17 задание, только тесты не проходятся, это как-то связано с пайчармом и его настройкой. В люом случае вводя эту комманду: python -m test_runner tests/ все тесты проходятся

--- schedule_pack/conveyor_schedule.py
+++ schedule_pack/conveyor_schedule.py
@@ -55,13 +55,61 @@ class ConveyorSchedule(AbstractSchedule):
     def __fill_schedule(self, tasks: list[StagedTask]) -> None:
         """Процедура составляет расписание из элементов ScheduleItem для каждого
         исполнителя, согласно алгоритму Джонсона."""
-        pass
+        primary_schedule = self._executor_schedule[0]
+        auxiliary_schedule = self._executor_schedule[1]
+
+        start_time = 0
+
+        for activity in tasks:
+            phase_length = activity.stage_duration(0)
+            primary_schedule.append(ScheduleItem(activity, start_time, phase_length))
+            start_time += phase_length
+
+        current_task_index = next_task_index = 0
+        cumulative_time = 0
+
+        while next_task_index < len(tasks):
+            schedule_entry = primary_schedule[next_task_index]
+            completion_time = schedule_entry.end
+
+            if schedule_entry.task_name == tasks[current_task_index].name and completion_time > cumulative_time:
+                idle_duration = abs(completion_time - cumulative_time)
+                auxiliary_schedule.append(ScheduleItem(None, cumulative_time, idle_duration, True))
+                cumulative_time += idle_duration
+            else:
+                task_duration = tasks[current_task_index].stage_duration(1)
+                auxiliary_schedule.append(ScheduleItem(tasks[current_task_index], cumulative_time, task_duration))
+                cumulative_time += task_duration
+                current_task_index += 1
+
+            if cumulative_time >= completion_time:
+                next_task_index += 1
+
+        final_task = tasks[-1]
+        auxiliary_schedule.append(ScheduleItem(final_task, cumulative_time, final_task.stage_duration(1)))
+        primary_schedule.append(ScheduleItem(None, primary_schedule[-1].end,
+                                             auxiliary_schedule[-1].end - primary_schedule[-1].end, True))
 
     @staticmethod
     def __sort_tasks(tasks: list[StagedTask]) -> list[StagedTask]:
         """Возвращает отсортированный список задач для применения
         алгоритма Джонсона."""
-        pass
+        primary_tasks = []
+        secondary_tasks = []
+
+        for item in tasks:
+            initial_duration, later_duration = item.stage_durations
+
+            if initial_duration <= later_duration:
+                primary_tasks.append(item)
+            else:
+                secondary_tasks.append(item)
+
+        primary_tasks.sort(key=lambda task: task.stage_duration(0))
+        secondary_tasks.sort(key=lambda task: task.stage_duration(1), reverse=True)
+
+        sorted_tasks = primary_tasks + secondary_tasks
+        return sorted_tasks
 
     @staticmethod
     def __validate_params(tasks: list[StagedTask]) -> None:
