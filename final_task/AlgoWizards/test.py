a465b7897947be165b1b31b8f53a577bdb7d754a
MikhailovAlexander <avmikhaylov_3@edu.hse.ru>
2024-05-16 23:16:21 +0500

    Добавил заготовку для финального задания

---
+++ final_task/AlgoWizards/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class Test(unittest.TestCase):
+    """Это будет класс для тестирования разработанного алгоритма"""
+
+    def test_something(self):
+        """Здесь будут тест кейсы тестирования разработанного алгоритма"""
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
