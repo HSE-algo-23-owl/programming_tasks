5adb9396b707ad60d3e18856598f8908c2df134c
grzegorz231 <vvkhazhiev@edu.hse.ru>
2024-06-17 17:26:21 +0500

    исправили критерии тестирования, сделали тесты и реализовали алгоритм

--- final_task/blur/main.py
+++ final_task/blur/main.py
@@ -1,7 +1,38 @@
-# Здесь будет исходный код разработанного алгоритма
+from collections import deque
+
+ERROR_EMPTY_GRAPH = 'Граф пустой'
+ERROR_START_NODE_NOT_IN_GRAPH = 'Стартовая вершина не находится в графе'
+
+
+def bfs(graph, start):
+    if not graph:
+        raise ValueError(ERROR_EMPTY_GRAPH)
+    if start not in graph:
+        raise ValueError(ERROR_START_NODE_NOT_IN_GRAPH)
+
+    visited = []
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            for neighbour in graph[node]:
+                if neighbour not in visited:
+                    queue.append(neighbour)
+
+    return visited
+
 
 def main():
-    print(f'Здесь будет пример использования разработанного алгоритма')
+    graph = {'A': ['B', 'C'],
+             'B': ['A', 'D', 'E'],
+             'C': ['A', 'B', 'E'],
+             'D': ['B'],
+             'E': ['B', 'C']
+             }
+    start = 'B'
+    print(bfs(graph, start))
 
 
 if __name__ == '__main__':
--- final_task/blur/test.py
+++ final_task/blur/test.py
@@ -1,5 +1,5 @@
 import unittest
-
+from main import bfs, ERROR_START_NODE_NOT_IN_GRAPH, ERROR_EMPTY_GRAPH
 
 class Test(unittest.TestCase):
     """Это будет класс для тестирования разработанного алгоритма"""
@@ -8,6 +8,78 @@ class Test(unittest.TestCase):
         """Здесь будут тест кейсы тестирования разработанного алгоритма"""
         self.assertEqual(1, 1)
 
+    def test_empty_graph(self): #сделано
+        graph = {}
+        start = 'A'
+        with self.assertRaises(ValueError) as err:
+            bfs(graph, start)
+        self.assertEqual(ERROR_EMPTY_GRAPH, str(err.exception))
+
+    def test_single_node_graph(self):
+        graph = {'A': []}
+        start = 'A'
+        self.assertEqual(bfs(graph, start), ['A'])
+
+    def test_graph_without_edges(self):
+        graph = {'A': [], 'B': [], 'C': []}
+        start = 'A'
+        self.assertEqual(bfs(graph, start), ['A'])
+
+    def test_graph_with_cycles(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['A', 'D', 'E'],
+            'C': ['A', 'F'],
+            'D': ['B'],
+            'E': ['B', 'F'],
+            'F': ['C', 'E']
+        }
+        start = 'A'
+        self.assertEqual(bfs(graph, start), ['A', 'B', 'C', 'D', 'E', 'F'])
+
+    def test_graph_with_custom_data(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D'],
+            'C': ['E'],
+            'D': ['F'],
+            'E': ['F'],
+            'F': []
+        }
+        start = 'A'
+        self.assertEqual(bfs(graph, start), ['A', 'B', 'C', 'D', 'E', 'F'])
+
+    def test_order_of_nodes(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D', 'E'],
+            'C': ['F'],
+            'D': [],
+            'E': ['F'],
+            'F': []
+        }
+        start = 'A'
+        self.assertEqual(bfs(graph, start), ['A', 'B', 'C', 'D', 'E', 'F'])
+
+    def test_graph_is_none(self):
+        graph = None
+        start = 'A'
+        with self.assertRaises(ValueError):
+            bfs(graph, start)
+
+    def test_start_node_not_in_graph(self):
+        graph = {
+            'A': ['B', 'C'],
+            'B': ['D'],
+            'C': ['E'],
+            'D': ['F'],
+            'E': ['F'],
+            'F': []
+        }
+        start = 'G'
+        with self.assertRaises(ValueError) as err:
+            bfs(graph, start)
+        self.assertEqual(ERROR_START_NODE_NOT_IN_GRAPH, str(err.exception))
 
 if __name__ == '__main__':
     unittest.main()
