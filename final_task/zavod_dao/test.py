b7ce124734afc434f9e563fd7770362070445d55
fedorokonma <113138852+fedorokonma@users.noreply.github.com>
2024-06-21 22:10:43 +0500

    добавили тесты

--- final_task/zavod_dao/test.py
+++ final_task/zavod_dao/test.py
@@ -1,13 +1,38 @@
 import unittest
 
 
-class Test(unittest.TestCase):
-    """Это будет класс для тестирования разработанного алгоритма"""
+class TestSimplexMethod(unittest.TestCase):
+    def test_standard_maximization(self):
+        c = np.array([3, 2])
+        A = np.array([[1, 2], [2, 1]])
+        b = np.array([8, 10])
+        solution, objective = simplex_method(c, A, b)
+        np.testing.assert_array_almost_equal(solution, [4, 2])
+        self.assertAlmostEqual(objective, 16)
 
-    def test_something(self):
-        """Здесь будут тест кейсы тестирования разработанного алгоритма"""
-        self.assertEqual(1, 1)
+    def test_maximization_integer_coefficients(self):
+        c = np.array([5, 4, 3])
+        A = np.array([[2, 3, 1], [4, 1, 2], [3, 4, 2]])
+        b = np.array([5, 11, 8])
+        solution, objective = simplex_method(c, A, b)
+        np.testing.assert_array_almost_equal(solution, [2, 0, 1])
+        self.assertAlmostEqual(objective, 13)
 
+    def test_maximization_equality_constraints(self):
+        c = np.array([4, 6])
+        A = np.array([[2, 3], [1, 1]])
+        b = np.array([6, 3])
+        solution, objective = simplex_method(c, A, b)
+        np.testing.assert_array_almost_equal(solution, [0, 2])
+        self.assertAlmostEqual(objective, 12)
+
+    def test_unbounded_solution(self):
+        c = np.array([1, 1])
+        A = np.array([[-1, 1]])
+        b = np.array([1])
+        solution, objective = simplex_method(c, A, b)
+        self.assertIsNone(solution)
+        self.assertIsNone(objective)
 
 if __name__ == '__main__':
     unittest.main()
